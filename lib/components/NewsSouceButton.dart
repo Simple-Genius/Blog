d0f676c58a9d7f79ea747c64e9009b405d55ad60
Boye <ajila.adeboye@gmail.com>
2023-02-15 12:27:08 +0100

    Still on the buttons

--- lib/components/NewsSouceButton.dart
+++ lib/components/NewsSouceButton.dart
@@ -18,28 +18,35 @@ class _NewsSourceButtonState extends State<NewsSourceButton> {
 
   Color backgroundColor = Colors.white;
   Color textColor = darkBlue;
+  bool isEnabled = false;
+
 
 
   @override
   Widget build(BuildContext context) {
+
+    void toggleEnabled(){
+     isEnabled = isEnabled == false ? true : false;
+    }
+
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: OutlinedButton(
         style: OutlinedButton.styleFrom(
-          backgroundColor: backgroundColor,
+          backgroundColor: isEnabled == true ? darkBlue : Colors.white,
           shape:  RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(30)
           ),
         ),
         onPressed: (){
           setState(() {
-
+            toggleEnabled();
           });
         },
         child:  Text(
          widget.newsSource,
           style:  TextStyle(
-              color: textColor
+              color: isEnabled == true ? Colors.white : darkBlue
           ),
         ),
       ),
