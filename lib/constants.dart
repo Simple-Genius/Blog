6d49cc9aabaeeda4c04a34eabcee443015548263
Simple-Genius <ajila.adeboye@gmail.com>
2023-02-19 22:30:29 +0100

    buttons taking time

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,7 +3,6 @@ import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 
 const darkBlue = Color(0xff041029);
-final _controller = ValueNotifier<bool>(false);
 const text ="""Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean porta lectus non pretium hendrerit. Suspendisse potenti. Nunc ex arcu, fringilla ac metus fringilla, aliquet ultricies libero. Donec maximus leo bibendum dictum convallis. Vivamus ac purus sit amet lorem sagittis feugiat. Praesent efficitur consectetur velit, et tempus ipsum aliquam eget."
 
     "In id mauris nec lectus hendrerit faucibus vitae eget quam. Curabitur sagittis felis sed arcu lacinia cursus. Nunc est erat, tincidunt in erat at, faucibus iaculis est. Mauris a sodales diam. Phasellus pretium, sem id iaculis tincidunt, tortor massa tempus ipsum, sit amet sodales est lacus eget justo. Vestibulum lacinia lacus suscipit est egestas condimentum. Sed interdum rutrum urna, quis interdum nisi ultrices id. Vestibulum vel iaculis libero, ut molestie augue.Morbi posuere massa sit amet sem suscipit ultrices eget id neque. Proin ut varius erat. Duis efficitur, sapien et faucibus commodo, leo nisi eleifend nunc, quis scelerisque felis turpis a ligula. Etiam vel vulputate erat. "
